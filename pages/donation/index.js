736b6499c99eedc1cee0420a04271898b7d394d7
Milad Azami <miladazami120@gmail.com>
2021-09-12 00:34:48 +0430

    Donation Page added to project

--- pages/donation/index.js
+++ pages/donation/index.js
@@ -1,15 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
 
 import Layout from '../../common/Layout/Layout';
 
+import styles from './index.module.css';
+
 const Donation = () => {
   return (
-    <div>
-      <Layout>
-        <h1>Buy me a coffe</h1>
-      </Layout>
-    </div>
+    <Layout>
+      <div className={styles.mainContainer}>
+        <div className={styles.container}>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+    </Layout>
   );
 };
 
 export default Donation;
+
+const Card = () => {
+  const [button, setButton] = useState('Copy');
+
+  useEffect(() => {
+    setTimeout(() => {
+      setButton('Copy');
+    }, 2000);
+  }, [button]);
+
+  const click = () => {
+    navigator.clipboard.writeText('bnb1m4jkycgwhttkzspehl5dcxjgxew7hhfe80627r');
+    setButton('Copied!');
+  };
+
+  return (
+    <div className={styles.card}>
+      <h3>BNB Wallet</h3>
+      <Image
+        src="/assets/images/bnbwallet.jpg"
+        alt="bnb wallet"
+        height="200"
+        width="200"
+      />
+      <p>bnb1m4jkycgwhttkzspehl5dcxjgxew7hhfe80627r</p>
+      <button onClick={click}>{button}</button>
+    </div>
+  );
+};
