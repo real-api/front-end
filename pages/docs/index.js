ec7cc29bd8fc4423495e1ec4186bcb2f6998ffe1
Milad Azami <miladazami120@gmail.com>
2021-09-15 17:59:16 +0430

    Basic structure of docs page is created

--- pages/docs/index.js
+++ pages/docs/index.js
@@ -3,6 +3,9 @@ import React from 'react';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import Header from '../../common/Layout/Header/Header';
 import Footer from '../../common/Layout/Footer/Footer';
+import CodeTemplate from '../../common/CodeTemplate/CodeTemplate';
+
+import { getAllProducts } from '../../constant/code';
 
 import styles from './index.module.css';
 
@@ -16,7 +19,18 @@ const Docs = () => {
             <Header />
           </nav>
           <main className={styles.contentContainer}>
-            <h1>Milad</h1>
+            <section className={styles.docsSection}>
+              <p>
+                fakeStoreApi can be used with any type of shopping project that
+                needs products, carts, and users in JSON format. you can use
+                examples below to check how fakeStoreApi works and feel free to
+                enjoy it in your awesome projects!
+              </p>
+            </section>
+            <section className={styles.docsSection}>
+              <h2>Products</h2>
+              <CodeTemplate text={getAllProducts} />
+            </section>
           </main>
         </div>
       </div>
