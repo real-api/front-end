06fb1b18559d1eacbba71ba0a092c5a954d7623d
Milad Azami <miladazami120@gmail.com>
2021-09-20 14:52:05 +0430

    side bar of fake api is customized

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -5,31 +5,28 @@ import styles from './Sidebar.module.css';
 const Sidebar = () => {
   return (
     <aside className={styles.container}>
-      <ul>
-        <li className={styles.headerItem}>Authentication</li>
-        <li className={styles.item}>Register</li>
-        <li className={styles.item}>Login</li>
-      </ul>
-      <ul>
+      <h3 className={styles.headerItem}>Fake API</h3>
+      <ul className={styles.firstUl}>
         <li className={styles.headerItem}>
           <a href="#blogs">Blogs</a>
         </li>
-        <li className={styles.item}>Create blog</li>
-        <li className={styles.item}>Get all blogs</li>
-        <li className={styles.item}>Get blog by ID</li>
-        <li className={styles.item}>Update blog</li>
-        <li className={styles.item}>Remove blog by ID</li>
+        <li className={styles.item}>Get all fake blogs</li>
+        <li className={styles.item}>Get fake blog by ID</li>
+        <li className={styles.item}>Delete fake blog by ID</li>
       </ul>
       <ul>
         <li className={styles.headerItem}>
           <a href="#comments">Comments</a>
         </li>
-        <li className={styles.item}>Send comment</li>
-        <li className={styles.item}>Get all comments</li>
-        <li className={styles.item}>Get blog comments</li>
-        <li className={styles.item}>Reply comment</li>
-        <li className={styles.item}>Confirm comment</li>
-        <li className={styles.item}>Remove comment</li>
+        <li className={styles.item}>Get all fake comments</li>
+        <li className={styles.item}>Get fake comment by ID</li>
+        <li className={styles.item}>Delete fake comment by ID</li>
+      </ul>
+      <h3 className={styles.headerItem}>Real API</h3>
+      <ul>
+        <li className={styles.headerItem}>Authentication</li>
+        <li className={styles.item}>Register</li>
+        <li className={styles.item}>Login</li>
       </ul>
     </aside>
   );
